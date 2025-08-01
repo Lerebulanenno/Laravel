4e15bf0bb3f7ddfdae645577eb5c77485cdfc3b6
Enno Lerebulan <lerebulanenno@gmail.com>
2025-08-01 10:47:22 +0800

    menambahkan halaman login serta css

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login - Balimart</title>
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Jaro&display=swap" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
+</head>
+<body>
+
+
+        <!-- Navbar -->
+    <header class="navbar">
+        <div class="logo">BALIM<span><i class="fa-solid fa-bag-shopping"></i></span>RT</div>
+
+        <div class="search-container">
+            <input type="text" placeholder="Cari produk...">
+            <button type="submit">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+
+        <div class="icon-group">
+            <span><i class="fa-solid fa-cart-shopping"></i></span>
+            <span><i class="fa-solid fa-user"></i></span>
+            <span><i class="fa-solid fa-globe"></i></span>
+        </div>
+    </header>
+
+    <div class="login-wrapper">
+    <!-- Kiri -->
+    <div class="login-left">
+      <img src="{{ asset('images/login.png') }}" alt="Ilustrasi Belanja" />
+      <h2>Temukan Pilihan Terbaikmu!</h2>
+    </div>
+
+    <!-- Kanan -->
+    <div class="login-right">
+      <div class="login-card">
+        <h2>Log in</h2>
+        <form method="POST" action="{{ route('login') }}">
+          @csrf
+
+          <input type="email" name="email" placeholder="Nama" required />
+          <input type="password" name="password" placeholder="Password" required />
+
+          <div class="login-links">
+            <a href="{{ route('password.request') }}">Lupa Password</a>
+          </div>
+
+          <button type="submit">Masuk</button>
+
+          <p class="register-text">Baru diBalimart? <a href="{{ route('register') }}">Daftar</a></p>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
